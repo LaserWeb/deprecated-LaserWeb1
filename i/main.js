f72d1dc833c7a7308ace2bac0c7b3c85c3eb8b63
Peter van der Walt <peter.plaaswerf@gmail.com>
2015-12-15 19:32:50 +0200

    Added Scale, Translate, Flip functions. Also fixed bug in 3D viewer with adding multiple helper objects on refreh

--- i/main.js
+++ i/main.js
@@ -28,6 +28,17 @@
 // Specific to your machine.  Also see same config in ui.js
 
 
+// add some functions to the string object
+String.prototype.trim = function() {
+	return this.replace(/^\s+|\s+$/g,"");
+}
+String.prototype.ltrim = function() {
+	return this.replace(/^\s+/,"");
+}
+String.prototype.rtrim = function() {
+	return this.replace(/\s+$/,"");
+}
+
 
 $(document).ready(function() {
 
@@ -221,6 +232,174 @@ $(document).ready(function() {
 			$('#clearQ').addClass('disabled');
 		}
 	});
+
+
+	// Gcode Scaling and Translate: from http://eng-serve.com/cnc/gcode_scale.html -->
+	function translateaxis(value,match)
+	{
+		newValue=fd((match*1.0)+(value*1.0));
+
+		return newValue;
+	}
+	function scaleaxis(value,match)
+	{
+		newValue=fd((match*1.0)*(value*1.0));
+
+		return newValue;
+	}
+	function replacechar(match)
+	{
+		newValue=match;
+		newValue=match.replace(/[0-9|.]+/,function(m){return replacenumber(m)});
+		return newValue; //"Goo["+match+"]";
+	}
+	function translate(axis,value,match)
+	{
+		newValue=match;
+		newValue=match.replace(/-?[0-9|.]+/,function(m){return translateaxis(value,m)});
+		return newValue; //"Goo["+match+"]";
+	}
+	function scale(axis,value,match)
+	{
+		newValue=match;
+		newValue=match.replace(/[0-9|.]+/,function(m){return scaleaxis(value,m)});
+		return newValue; //"Goo["+match+"]";
+	}
+
+	function process180()
+	{
+		// [X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+
+
+		theInput=document.getElementById("gcodepreview").value;
+		theOutput=document.getElementById("gcodepreview");
+		//theInput=theInput.replace(/[X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+/g,function(m){return replacechar(m)});
+		scaleX= -1;
+		scaleY= -1;
+		//scaleZ=document.getElementById("ZSCALE").value;
+		scaleZ=1
+
+		theInput=theInput.replace(/[X|x]\s*-?[0-9|.]+/g,function(m){return scale("X",scaleX,m)});
+		theInput=theInput.replace(/[Y|y]\s*-?[0-9|.]+/g,function(m){return scale("Y",scaleY,m)});
+		theInput=theInput.replace(/[Z|Z]\s*-?[0-9|.]+/g,function(m){return scale("Z",scaleZ,m)});
+		theInput=theInput.replace(/[I|i]\s*-?[0-9|.]+/g,function(m){return scale("I",scaleX,m)});
+		theInput=theInput.replace(/[J|j]\s*-?[0-9|.]+/g,function(m){return scale("J",scaleY,m)});
+
+		transX=document.getElementById("BBXMAX").value;
+		transY=document.getElementById("BBYMAX").value;
+		//transZ=document.getElementById("ZTRANS").value;
+		transZ=0
+
+		theInput=theInput.replace(/[X|x]\s*-?[0-9|.]+/g,function(m){return translate("X",transX,m)});
+		theInput=theInput.replace(/[Y|y]\s*-?[0-9|.]+/g,function(m){return translate("Y",transY,m)});
+		theInput=theInput.replace(/[Z|z]\s*-?[0-9|.]+/g,function(m){return translate("Z",transZ,m)});
+
+
+		// I and J are always relative so no need to translate right?
+		//theInput=theInput.replace(/[I|i].+[0-9|.]+/g,function(m){return translate("I",transX,m)});
+		//theInput=theInput.replace(/[J|j].+[0-9|.]+/g,function(m){return translate("J",transY,m)});
+		theOutput.value=theInput;
+		//textarea.value=outputstring;
+	}
+
+	function processScale()
+	{
+		// [X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+
+
+		theInput=document.getElementById("gcodepreview").value;
+		theOutput=document.getElementById("gcodepreview");
+		//theInput=theInput.replace(/[X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+/g,function(m){return replacechar(m)});
+		scaleX=document.getElementById("scaleFactor").value / 100;
+		scaleY=document.getElementById("scaleFactor").value / 100;
+		//scaleZ=document.getElementById("ZSCALE").value;
+		scaleZ=1
+
+		theInput=theInput.replace(/[X|x]\s*-?[0-9|.]+/g,function(m){return scale("X",scaleX,m)});
+		theInput=theInput.replace(/[Y|y]\s*-?[0-9|.]+/g,function(m){return scale("Y",scaleY,m)});
+		theInput=theInput.replace(/[Z|Z]\s*-?[0-9|.]+/g,function(m){return scale("Z",scaleZ,m)});
+		theInput=theInput.replace(/[I|i]\s*-?[0-9|.]+/g,function(m){return scale("I",scaleX,m)});
+		theInput=theInput.replace(/[J|j]\s*-?[0-9|.]+/g,function(m){return scale("J",scaleY,m)});
+
+
+		// I and J are always relative so no need to translate right?
+		//theInput=theInput.replace(/[I|i].+[0-9|.]+/g,function(m){return translate("I",transX,m)});
+		//theInput=theInput.replace(/[J|j].+[0-9|.]+/g,function(m){return translate("J",transY,m)});
+		theOutput.value=theInput;
+		//textarea.value=outputstring;
+	}
+
+	function processTranslate()
+	{
+		// [X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+
+
+		theInput=document.getElementById("gcodepreview").value;
+		theOutput=document.getElementById("gcodepreview");
+		//theInput=theInput.replace(/[X|x|Y|y|Z|z|I|i|J|j].[0-9|.]+/g,function(m){return replacechar(m)});
+
+		transX=document.getElementById("XTRANS").value;
+		transY=document.getElementById("YTRANS").value;
+		//transZ=document.getElementById("ZTRANS").value;
+		transZ=0
+
+		theInput=theInput.replace(/[X|x]\s*-?[0-9|.]+/g,function(m){return translate("X",transX,m)});
+		theInput=theInput.replace(/[Y|y]\s*-?[0-9|.]+/g,function(m){return translate("Y",transY,m)});
+		theInput=theInput.replace(/[Z|z]\s*-?[0-9|.]+/g,function(m){return translate("Z",transZ,m)});
+	
+		// I and J are always relative so no need to translate right?
+		//theInput=theInput.replace(/[I|i].+[0-9|.]+/g,function(m){return translate("I",transX,m)});
+		//theInput=theInput.replace(/[J|j].+[0-9|.]+/g,function(m){return translate("J",transY,m)});
+		theOutput.value=theInput;
+		//textarea.value=outputstring;
+	}
+
+	function fd(number)
+	{
+		return number_format(number,6,".","");
+	}
+	function number_format( number, decimals, dec_point, thousands_sep ) 
+	{
+	      // http://kevin.vanzonneveld.net
+	      // + original by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	      // + improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	      // + bugfix by: Michael White (http://crestidg.com)
+	      // + bugfix by: Benjamin Lupton
+	      // + bugfix by: Allan Jensen (http://www.winternet.no)
+	      // + revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+	      // * example 1: number_format(1234.5678, 2, '.', '');
+	      // * returns 1: 1234.57
+	      var n = number, c = isNaN(decimals = Math.abs(decimals)) ? 2 : decimals;
+	      var d = dec_point == undefined ? "," : dec_point;
+	      var t = thousands_sep == undefined ? "." : thousands_sep, s = n < 0 ? "-" : "";
+	      var i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+	      return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+	}
+
+
+	$('#scale').on('click', function() {
+		processScale();
+		document.getElementById('scaleFactor').value = '100';
+		openGCodeFromText();
+	});
+
+	$('#translate').on('click', function() {
+		processTranslate();
+		document.getElementById('scaleFactor').value = '100';
+		document.getElementById('XTRANS').value = '0';
+		document.getElementById('YTRANS').value = '0';
+		document.getElementById('ZTRANS').value = '0';
+		openGCodeFromText();
+	});
+
+	$('#flip').on('click', function() {
+		process180();
+		document.getElementById('scaleFactor').value = '100';
+		document.getElementById('XTRANS').value = '0';
+		document.getElementById('YTRANS').value = '0';
+		document.getElementById('ZTRANS').value = '0';
+		openGCodeFromText();
+	});
+
+
+	// End of Scaling Function
 
 
 	
@@ -685,9 +864,9 @@ $(document).ready(function() {
 			$('#mX').html('X: '+posArray[4]);
 			$('#mY').html('Y: '+posArray[6]);
 			$('#mZ').html('Z: '+posArray[8]);
-			cylinder.position.x = (parseInt(posArray[2],10) - (laserxmax /2));
-			cylinder.position.y = (parseInt(posArray[4],10) - (laserymax /2));
-			cylinder.position.z = (parseInt(posArray[6],10) + 20);
+			cylinder.position.x = (parseInt(posArray[4],10) - (laserxmax /2));
+			cylinder.position.y = (parseInt(posArray[6],10) - (laserymax /2));
+			cylinder.position.z = (parseInt(posArray[8],10) + 20);
 	});
 	
 	
@@ -750,3 +929,5 @@ $(document).ready(function() {
 	
 	
 });
+
+
