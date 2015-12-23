d6232d7eae02237953782dba20ee7fa559e0e7fb
Dana Woodman <dana@danawoodman.com>
2015-12-23 13:08:53 -0800

    Allow overriding of config with environment variables.
    In order to let end users customize their environment, Ive added the ability to override the basic settings via environment variables in `config.js` by adding ` .env` file to the directory that overrides settings using dotenv.

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
+require('dotenv').load()
 
 var config = {};
 
-config.webPort = 8000;
-config.serialBaudRate = 115200;
-config.webcamPort = 8080;  // expects a webcam stream from mjpg_streamer
-config.xmax = 600 // Max length of X Axis in mm
-config.ymax = 400 // Max length of Y Axis in mm
-
+config.webPort = process.env.WEB_PORT || 8000;
+config.serialBaudRate = process.env.SERIAL_BAUD_RATE || 115200;
+config.webcamPort = process.env.WEBCAM_PORT || 8080;  // expects a webcam stream from mjpg_streamer
+config.xmax = process.env.X_MAX || 600 // Max length of X Axis in mm
+config.ymax = process.env.Y_MAX || 400 // Max length of Y Axis in mm
 
 module.exports = config;
